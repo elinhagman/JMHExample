e37eb32ae429976280ba94d51a26ca4f7f73f59e
dubbell <46517201+dubbell@users.noreply.github.com>
2023-03-14 04:33:43 +0100

    added main, added regex to yaml

--- src/main/java/org/linulun/ExampleJMH.java
+++ src/main/java/org/linulun/ExampleJMH.java
@@ -6,6 +6,14 @@ import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.runner.*;
+import org.openjdk.jmh.runner.options.*;
+
+import java.io.PrintStream;
+import java.util.*;
+import java.io.*;
+import org.openjdk.jmh.results.*;
+import org.openjdk.jmh.results.format.*;
 
 public class ExampleJMH {
 
@@ -14,6 +22,23 @@ public class ExampleJMH {
     @OutputTimeUnit(TimeUnit.MICROSECONDS)
     public void measureSingleShot() throws InterruptedException {
         TimeUnit.MILLISECONDS.sleep(100);
+    }
+
+    public static void main(String[] args) throws Exception{
+        Options opt = new OptionsBuilder().include(ExampleJMH.class.getSimpleName()).forks(1).verbosity(VerboseMode.SILENT).build();
+
+        Collection<RunResult> result = new Runner(opt).run();
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+
+        ResultFormat resultFormat = ResultFormatFactory.getInstance(ResultFormatType.JSON, printStream);
+        resultFormat.writeOut(result);
+
+        
+        String output = outputStream.toString("UTF8");
+
+        System.out.println(output);
     }
 
 }
